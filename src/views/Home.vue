037260bbb8d8eae2be1deae9a086843be5d4a12c
Fariz313m <fariz313.akbar@gmail.com>
2022-04-02 13:21:20 +0700

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,70 @@
 <template>
-  <div class="home">  
-    <h1>Selamat Datang Di Laundry Atma</h1>
+  <div class="home vh-100">
+    <h1>Selamat Datang Di Laundry Aqila</h1>
+    <div class="row w-100 justify-content-between">
+      <div class="col-md-3">
+        <div class="card bg-primary text-white px-5 py-2 rounded">
+          <h4>Transaksi</h4>
+          <h1>{{ transaction }}</h1>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card bg-primary text-white px-5 py-2 rounded">
+          <h4>Member</h4>
+          <h1>{{ member }}</h1>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card bg-primary text-white px-5 py-2 rounded">
+          <h4>Paket</h4>
+          <h1>{{ paket }}</h1>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card bg-primary text-white px-5 py-2 rounded">
+          <h4>Outlet</h4>
+          <h1>{{ outlet }}</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      paket: 0,
+      member: 0,
+      transaction: 0,
+      outlet: 0,
+    };
+  },
+  methods: {
+    fetch() {
+      this.axios.get("/member").then((res) => {
+        this.member = res.data.data.count;
+      });
+      this.axios.get("/transaksi").then((res) => {
+        this.transaction = res.data.data.count;
+      });
+      this.axios.get("/outlet").then((res) => {
+        this.outlet = res.data.data.count;
+      });
+      this.axios.get("/paket").then((res) => {
+        this.paket = res.data.data.count;
+      });
+    },
+  },
+  mounted() {
+    this.fetch();
+  },
+};
 </script>
--- src/views/Transaksi/Transaksi.vue
+++ src/views/Transaksi/Transaksi.vue
@@ -24,7 +24,12 @@
             <td>{{ item.user.name }}</td>
             <td>{{ item.batas_waktu }}</td>
             <td>
-              <select @change="update($event,item.id_transaksi,'status')" :disabled="loading" class="form-control" id="">
+              <select
+                @change="update($event, item.id_transaksi, 'status')"
+                :disabled="loading"
+                class="form-control"
+                id=""
+              >
                 <option :selected="item.status == 'baru'" value="baru">
                   baru
                 </option>
@@ -40,17 +45,34 @@
               </select>
             </td>
             <td>
-              <select @change="update($event,item.id_transaksi,'dibayar')" :disabled="loading" class="form-control">
+              <select
+                @change="update($event, item.id_transaksi, 'dibayar')"
+                :disabled="loading"
+                class="form-control"
+              >
                 <option :selected="item.dibayar == 'dibayar'" value="dibayar">
                   dibayar
                 </option>
-                <option :selected="item.dibayar == 'belum_dibayar'" value="belum_dibayar">
+                <option
+                  :selected="item.dibayar == 'belum_dibayar'"
+                  value="belum_dibayar"
+                >
                   belum dibayar
                 </option>
               </select>
             </td>
             <td>{{ item.tgl_bayar }}</td>
             <td>
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+                @click="showDetail(item)"
+              >
+                Launch demo modal
+              </button>
+
               <router-link
                 :to="'/transaksi/create?id=' + item.id_transaksi"
                 class="btn mx-2 btn-warning"
@@ -67,14 +89,164 @@
         </tbody>
       </table>
     </div>
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">INVOICE #{{transaksiDetail.id_transaksi}}</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body" id="invoice">
+            <h1>Aqila Laundry</h1>
+            <table class="text-start w-100" border="0">
+              <tr>
+                <td>
+                  ID Invoice
+                </td>
+                <td>
+                  :
+                </td>
+                <td>
+                  {{transaksiDetail.id_transaksi}}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Tanggal Transaksi
+                </td>
+                <td>
+                  :
+                </td>
+                <td>
+                  {{transaksiDetail.tgl}}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Nama Member
+                </td>
+                <td>
+                  :
+                </td>
+                <td>
+                  {{transaksiDetail.member.nama}}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Batas Waktu
+                </td>
+                <td>
+                  :
+                </td>
+                <td>
+                  {{transaksiDetail.batas_waktu}}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Tanggal Bayar
+                </td>
+                <td>
+                  :
+                </td>
+                <td>
+                  {{transaksiDetail.tgl_bayar}}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Status
+                </td>
+                <td>
+                  :
+                </td>
+                <td>
+                  {{transaksiDetail.status}}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Status Pembayaran
+                </td>
+                <td>
+                  :
+                </td>
+                <td>
+                  {{transaksiDetail.dibayar}}
+                </td>
+              </tr>
+            </table>
+            <table class="text-start w-100" border="0">
+              <thead>
+                <td>
+                  Jenis Paket
+                </td>
+                <td>
+                  Harga
+                </td>
+                <td>
+                  Jumlah
+                </td>
+                <td>
+                  Total Bayar
+                </td>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{{transaksiDetail.paket.jenis}}</td>
+                  <td>Rp.{{transaksiDetail.paket.harga}}</td>
+                  <td>{{transaksiDetail.berat}} KG</td>
+                  <td>Rp.{{transaksiDetail.berat * transaksiDetail.paket.harga}}</td>
+                </tr>
+              </tbody>
+            </table>
+            <p class="text-start">
+              1. Pengambilan barang harus menyertakan nota<br/>
+              2. Penyelesaian Pencucian Maks 3 Hari<br/>
+              3. Barang yang tidak diambil 1 bulan jika hilang bukan tanggung jawab kami<br/>
+              4. Apabila terjadi kehilangan atau cacat karena kesalahan kami, kami hanya bertanggung jawab 2x ongkos cuci <br/>
+            </p>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-danger text-white"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" @click="print" class="btn btn-primary">Print</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import print from "print-js";
+import printJS from 'print-js';
+
 export default {
   data() {
     return {
       transaksi: [],
-      loading: false
+      loading: false,
+      transaksiDetail:{
+        member:{},
+        paket:{}
+      }
     };
   },
   methods: {
@@ -88,19 +260,25 @@ export default {
         this.disabled(false);
       });
     },
-    update(event,id,type){
-      this.loading = true
-      let data  = {}
-      if(type=='status'){
-        data.status = event.target.value
-      }if(type=='dibayar'){
-        data.dibayar = event.target.value
+    update(event, id, type) {
+      this.loading = true;
+      let data = {};
+      if (type == "status") {
+        data.status = event.target.value;
       }
-      this.axios
-          .put("/transaksi/" + id, data)
-          .then(() => {
-            this.loading = false
-          });
+      if (type == "dibayar") {
+        data.dibayar = event.target.value;
+      }
+      this.axios.put("/transaksi/" + id, data).then(() => {
+        this.loading = false;
+      });
+    },
+    showDetail(transaksi){
+      this.transaksiDetail = transaksi;
+      console.log(transaksi);
+    },
+    print(){
+      printJS('invoice','html')
     },
     disabled(disabled = true) {
       if (disabled) {
@@ -132,3 +310,8 @@ export default {
   },
 };
 </script>
+<style>
+  td{
+    padding-bottom: 15px;
+  }
+</style>
